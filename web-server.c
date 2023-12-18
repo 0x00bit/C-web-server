34d602de21d3e23a39310ade377b6ce8f028e59b
magnstr <136848260+magnstr@users.noreply.github.com>
2023-12-18 16:59:14 -0300

    Add files via upload

--- web-server.c
+++ web-server.c
@@ -6,14 +6,9 @@
 #include <sys/sendfile.h>
 #include <unistd.h>
 #include <stdlib.h>
-/* hello, this is my first programm made in C language
- * this project  was make based in the video: Making
- * Minimalist Web Server in C on Linux so... credits for
- * Nir Lichtman, thanks!
- * made by magnstr
- */
 
-
+int sv_port = 0;
+int ip_addr = 0;
 
 void banner()
 {
@@ -55,36 +50,85 @@ void banner()
 );
 printf("server is listening on port 8080, in path %s\n",buf);
 }
-void main()
+
+int web_server()
 {
-	banner();	
-	int s = socket(AF_INET, SOCK_STREAM, 0);
-	
-	int sv_port = 8080;
-	int ip = 0;
+        int s = socket(AF_INET, SOCK_STREAM, 0);
 
-	struct sockaddr_in addr = {0};
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(sv_port);
-	addr.sin_addr.s_addr = 0;
-	
-	bind(s, (struct sockaddr *)&addr, sizeof(addr));
+        //int sv_port = 8080;
+        //int ip = 0;
 
-	listen(s, 10);
+        struct sockaddr_in addr = {0};
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(sv_port);
+        addr.sin_addr.s_addr = ip_addr;
 
-	int client_fd = accept(s, 0, 0);
+        bind(s, (struct sockaddr *)&addr, sizeof(addr));
 
-	char buffer[256] = {0};
-	recv(client_fd, buffer, 256, 0);
+        listen(s, 10);
 
-	char* f = buffer + 5;
-	*strchr(f, ' ') = 0;
-	int opened_fd = open(f, O_RDONLY);
-	sendfile(client_fd, opened_fd, 0, 256);
-	close(opened_fd);
-	close(client_fd);
-	close(s);
+        int client_fd = accept(s, 0, 0);
 
+        char buffer[256] = {0};
+        recv(client_fd, buffer, 256, 0);
+
+        char* f = buffer + 5;
+        *strchr(f, ' ') = 0;
+        int opened_fd = open(f, O_RDONLY);
+        sendfile(client_fd, opened_fd, 0, 256);
+        close(opened_fd);
+        close(client_fd);
+        close(s);
+        return 0;
 }
 
+int menu()
+{
+	int choice;
+
+	puts(
+	"[1] set server port (default 8080) \n"
+	"[2] set server ip addr \n"
+	"[3] run server \n"
+	);
+
+	printf("please, select an option: \n");
+	scanf("%d", &choice);
+	//printf("selected option: %d\n", choice);
+	
+	switch(choice){
+	    case 1:
+	        printf("type server port number: \n");
+		scanf("%d",&sv_port);
+		printf("port defined: %d\n",sv_port);
+		menu();
+		printf("\n");
+		break;
+
+
+	    case 2:
+                printf("type server ip addr: \n");
+                scanf("%d",&ip_addr);
+                printf("ip defined: %d\n",ip_addr);
+		printf("\n");
+	    	menu();
+		break;
+
+	    case 3:
+		web_server();
+		break;
+	    
+	    default:
+		printf("please select an valid option");
+
+	}
+	return 0;
+}
+
+int main()
+{
+    banner();
+    menu();
+    return 0;
+}
 
